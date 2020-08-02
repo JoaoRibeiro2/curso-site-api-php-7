fc83669c87bfff6c7697a62f4e0cd208fe9d0e6a
Felipe Renan <feliperenanvieira@gmail.com>
2020-08-02 18:15:32 -0300

    Removendo o diretório 09
    Removendo o diretório 09 temporariamente devido a uma mudança na ordem dos módulos do curso.

--- 09_MVC_Model_View_Controller/public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Kernel\Server;
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-new Server();
--- 09_MVC_Model_View_Controller/src/Service/Hello/IHelloService.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Service\Hello;
-
-interface IHelloService
-{
-    public function showMessage(): string;
-}
